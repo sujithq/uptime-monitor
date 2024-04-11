bc2457a21186266714255bb22ad701e86bd46610
Sujith Quintelier <sujith.quintelier@gmail.com>
2024-04-11 06:54:25 +0000

    Update packages

--- src/automations.ts
+++ src/automations.ts
@@ -23,7 +23,7 @@ const createAutomatedIssue = async () => {
       page,
     });
     for await (const repository of results.data.items) {
-      const owner = repository.owner.login;
+      const owner = repository?.owner?.login || "";
       const repo = repository.name;
       if (`${owner}/${repo}` !== "wakatime/statuspage") {
         let hasDisabledAutomatedIssues = false;
